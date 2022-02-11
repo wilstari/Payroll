3d93265a5fe665f2375c8f9620948f0cc50dc9ad
wilstari <34189191+wilstari@users.noreply.github.com>
2022-02-11 19:05:52 +0000

    round 1 of adding salt
    added method to generate salt,
    added method to open connection to user table
    minor updates to password hashing method

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -5,11 +5,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Arrays;
 
 
 public class LoginController {
@@ -26,8 +34,23 @@ public class LoginController {
     private TextField userText;
     @FXML
     private PasswordField passText;
+    byte[]salt=  createSalt();
 
-
+    public Connection getConnection() {
+    	Connection conn;
+    	String user ="root";
+    	String pwd="Password123";
+    	
+    	try {
+    		conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll",user,pwd);
+    		return conn;
+    	}catch(Exception ex) {
+    		
+    		
+    		return null;
+    	}
+    	
+    }
 
     public void userLogin(ActionEvent event) throws IOException {
     	checkLogin();
@@ -40,7 +63,9 @@ public class LoginController {
        
         if(userText.getText().toString().equals("test") && passText.getText().toString().equals("123")) {
         	errorlbl.setText("Success!");
-System.out.println(PasswordHashing("123"));
+System.out.println(PasswordHashing("321", salt));
+System.out.println(Arrays.toString(salt));
+getpass();
             m.changeScene("landingPage.fxml");
         }
 
@@ -56,11 +81,16 @@ System.out.println(PasswordHashing("123"));
         
     }
     
-   public static String PasswordHashing(String password){
+   public static String PasswordHashing(String password, byte[]salt){
 	   try {
 		MessageDigest messageDigest= MessageDigest.getInstance("MD5");
 		messageDigest.update(password.getBytes());
+		messageDigest.reset();
+		messageDigest.update(salt);
+		
 		byte[] resultByteArray= messageDigest.digest();
+		
+		
 		StringBuilder sb= new StringBuilder();
 		for (byte b: resultByteArray) {
 			sb.append(String.format("%02x",b));
@@ -75,5 +105,39 @@ System.out.println(PasswordHashing("123"));
     	
 	   
     }
+   
+   public static byte[] createSalt(){
+	  byte[] bytes= new byte[20];
+	  SecureRandom random = new SecureRandom();
+	  random.nextBytes(bytes);
+	  return bytes;
+   }
+   
+   public String getpass(){
+		
+		Connection conn = getConnection();
+		String query = "SELECT hash FROM userlogin WHERE id=1";
+		Statement st;
+		ResultSet rs;
+		String users=null;
+		
+		
+		try {
+		st=conn.createStatement();
+		rs=st.executeQuery(query);
+		if(rs.next()) {
+			users=rs.getString(1);
+		}
+	
+		
+			
+		}catch(Exception ex) {
+			System.out.println(ex);
+			return null;
+		}
+		System.out.println(users);		
+		return "";
+	} 
+   
 	
 }
